86cd0b19cc3d8ae49cf937bb09af19f078418991
Andrew Pratt <apratt957@gmail.com>
2020-05-20 12:29:04 -0500

    basic song switching implementation

--- src/pages/sounds.js
+++ src/pages/sounds.js
@@ -7,31 +7,37 @@ import Layout from "../components/layout"
 const Sounds = () => {
   let [songList, setSongList] = useState(songs)
   let [isPlaying, setIsPlaying] = useState(false)
+  let [currentSong, setCurrentSong] = useState("")
 
   const stopSong = () => {
     setIsPlaying(false)
+    setCurrentSong("")
     Tone.Transport.stop()
     Tone.Transport.cancel()
     Tone.context.close()
   }
 
-  const playSong = song => {
-    setIsPlaying(true)
-    song.makeSong()
+  const playSong = async song => {
+    if (isPlaying) {
+      await stopSong()
+      setIsPlaying(true)
+      setCurrentSong(song.name)
+      song.makeSong()
+    } else {
+      setIsPlaying(true)
+      setCurrentSong(song.name)
+      song.makeSong()
+    }
   }
   return (
     <Layout>
-      {songList.map(song => (
-        <button
-          key={song.name}
-          disabled={isPlaying}
-          onClick={() => playSong(song)}
-        >
-          Play {song.name}
-        </button>
-      ))}
-
-      <button onClick={stopSong}>stop song</button>
+      {songList.map(song => {
+        if (currentSong === song.name) {
+          return <button onClick={stopSong}>Stop</button>
+        } else {
+          return <button onClick={() => playSong(song)}>{song.name}</button>
+        }
+      })}
     </Layout>
   )
 }
