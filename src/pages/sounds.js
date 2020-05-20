4537289824f346b440b4adc3853de35c9f7fe031
Andrew Pratt <apratt957@gmail.com>
2020-05-20 12:14:58 -0500

    tone context debacle temporarily solved

--- src/pages/sounds.js
+++ src/pages/sounds.js
@@ -1,14 +1,37 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import Tone from "tone"
 import { songs } from "../songs"
 
 import Layout from "../components/layout"
 
 const Sounds = () => {
+  let [songList, setSongList] = useState(songs)
+  let [isPlaying, setIsPlaying] = useState(false)
+
+  const stopSong = () => {
+    setIsPlaying(false)
+    Tone.Transport.stop()
+    Tone.Transport.cancel()
+    Tone.context.close()
+  }
+
+  const playSong = song => {
+    setIsPlaying(true)
+    song.makeSong()
+  }
   return (
     <Layout>
-      {songs.map(song => (
-        <button onClick={song}>Play</button>
+      {songList.map(song => (
+        <button
+          key={song.name}
+          disabled={isPlaying}
+          onClick={() => playSong(song)}
+        >
+          Play {song.name}
+        </button>
       ))}
+
+      <button onClick={stopSong}>stop song</button>
     </Layout>
   )
 }
