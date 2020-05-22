8394846c17ef39f52a925ef459316e678d119d0b
Andrew Pratt <apratt957@gmail.com>
2020-05-21 20:16:16 -0500

    added css animation while playing

--- src/components/songCard.js
+++ src/components/songCard.js
@@ -1,33 +1,16 @@
 import React from "react"
 
 const SongCard = ({ song, playSong, stopSong, currentSong }) => {
-  const square = {
-    border: "2px solid black",
-    height: "300px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  }
-  const circle = {
-    border: "2px solid black",
-    height: "300px",
-    borderRadius: "50%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  }
   return (
-    <div style={song.name === currentSong ? circle : square}>
-      <h3>{song.name}</h3>
+    <div className={song.name === currentSong ? "playing" : "circle"}>
+      <p>{song.name}</p>
       {currentSong === song.name ? (
         <button key={song.name} onClick={stopSong}>
-          Stop
+          &#x25a0;
         </button>
       ) : (
         <button key={song.name} onClick={() => playSong(song)}>
-          Play
+          &#x25BA;
         </button>
       )}
     </div>
